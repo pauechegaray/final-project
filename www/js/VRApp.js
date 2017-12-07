b8292358475d6d35ee7067ff7265abb7a202c6f7
Diego Montesinos <chesp.gommont@gmail.com>
2017-12-06 20:47:39 -0600

    Clean project and put the preview.html to dev

--- www/js/VRApp.js
+++ www/js/VRApp.js
@@ -1,63 +1,25 @@
 (function (global) {
   'use strict';
 
-  var mesh;
-  var helper;
-
   function onSetup() {
-    var geometry = new THREE.BoxGeometry(10, 10, 10);
-    var material = new THREE.MeshPhongMaterial({
-      color : 0xff0000
-    });
-    mesh = new THREE.Mesh(geometry, material);
-    mesh.position.x = 0;
-    mesh.position.y = 7;
-    VRWorld.scene.add(mesh);
-
-    var light = new THREE.HemisphereLight(0x777777, 0x000000, 2.0);
-    VRWorld.scene.add(light);
-  
-    helper = new THREE.CameraHelper(VRWorld.vrCamera);
-    VRWorld.scene.add(helper);
-  
-    setupCheckboard();
+    /**
+     * YOUR SETUP CODE GOES HERE!
+     * 
+     * This function will be executed once the 3D world has been created.
+     * 
+     * The most important variables are available in:
+     * VRWorld.scene    - Is the scene of the world.
+     * VRWorld.camera   - Is the camera for the preview.
+     * VRWorld.vrCamera - The camera for VR view.
+     */
   }
 
-  function setupCheckboard() {
-    var loader = new THREE.TextureLoader();
-    loader.load(
-      'textures/patterns/checker.png',
-      function (texture) {
-        texture.wrapS = THREE.RepeatWrapping;
-        texture.wrapT = THREE.RepeatWrapping;
-        texture.repeat = new THREE.Vector2(50, 50);
-        texture.anisotropy = VRWorld.renderer.getMaxAnisotropy();
-  
-        var material = new THREE.MeshPhongMaterial({
-          color     : 0xffffff,
-          specular  : 0xffffff,
-          shininess : 60,
-          shading   : THREE.FlatShading,
-          map       : texture
-        });
-  
-        var geometry = new THREE.PlaneGeometry(1000, 1000);
-        var mesh = new THREE.Mesh(geometry, material);
-        mesh.rotation.x = -Math.PI / 2;
-        VRWorld.scene.add(mesh);
-      },
-      function (xhr) {
-        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-      },
-      function () {
-        console.log('Error loading checkboard');
-      }
-    );
-  }  
-
   function onUpdate() {
-    mesh.position.x += 0.1;
-    helper.update();
+    /**
+     * YOUR SETUP CODE GOES HERE!
+     * 
+     * This function will be executed every time the world was rendered.
+     */
   }
 
   var VRApp = {
--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,13 @@
 
 // Main method
 $(document).ready(function () {
+  
+  var onPreview = window.location.pathname.indexOf('preview.html') !== -1;
 
   VRWorld.setup({
     onSetup  : VRApp.onSetup,
     onUpdate : VRApp.onUpdate,
-    renderVR : false
+    renderVR : !onPreview
   });
 
   VRWorld.init();
